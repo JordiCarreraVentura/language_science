5db05035b5393db3b8357b1c06dba4f36bd458e8
Jordi Carrera Ventura <jordi.carrera.ventura@gmail.com>
2024-05-05 09:37:41 +0200

    Extend example and add jupyter notebook

--- ReFinED/refined_test.py
+++ ReFinED/refined_test.py
@@ -1,3 +1,6 @@
+from nltk import ngrams
+from nltk import sent_tokenize as splitter
+from nltk import wordpunct_tokenize as tokenizer
 from refined.inference.processor import Refined
 
 
@@ -10,7 +13,68 @@ refined = Refined.from_pretrained(model_name='wikipedia_model_with_numbers',
 #     print(span)
 
 
-spans = refined.process_text("From the 9th to the 12th centuries, Venice developed into a powerful maritime empire (an Italian thalassocracy known also as repubblica marinara). In addition to Venice there were seven others: the most important ones were Genoa, Pisa, and Amalfi; and the lesser known were Ragusa, Ancona, Gaeta and Noli. Its own strategic position at the head of the Adriatic made Venetian naval and commercial power almost invulnerable. With the elimination of pirates along the Dalmatian coast, the city became a flourishing trade centre between Western Europe and the rest of the world, especially with the Byzantine Empire and Asia, where its navy protected sea routes against piracy. The Republic of Venice seized a number of places on the eastern shores of the Adriatic before 1200, mostly for commercial reasons, because pirates based there were a menace to trade. The doge already possessed the titles of Duke of Dalmatia and Duke of Istria. Later mainland possessions, which extended across Lake Garda as far west as the Adda River, were known as the Terraferma; they were acquired partly as a buffer against belligerent neighbours, partly to guarantee Alpine trade routes, and partly to ensure the supply of mainland wheat (on which the city depended). In building its maritime commercial empire, Venice dominated the trade in salt, acquired control of most of the islands in the Aegean, including Crete, and Cyprus in the Mediterranean, and became a major power-broker in the Near East. By the standards of the time, Venice's stewardship of its mainland territories was relatively enlightened and the citizens of such towns as Bergamo, Brescia, and Verona rallied to the defence of Venetian sovereignty when it was threatened by invaders.")
+text = "From the 9th to the 12th centuries, Venice developed into a powerful maritime empire (an Italian thalassocracy known also as repubblica marinara). In addition to Venice there were seven others: the most important ones were Genoa, Pisa, and Amalfi; and the lesser known were Ragusa, Ancona, Gaeta and Noli. Its own strategic position at the head of the Adriatic made Venetian naval and commercial power almost invulnerable. With the elimination of pirates along the Dalmatian coast, the city became a flourishing trade centre between Western Europe and the rest of the world, especially with the Byzantine Empire and Asia, where its navy protected sea routes against piracy. The Republic of Venice seized a number of places on the eastern shores of the Adriatic before 1200, mostly for commercial reasons, because pirates based there were a menace to trade. The doge already possessed the titles of Duke of Dalmatia and Duke of Istria. Later mainland possessions, which extended across Lake Garda as far west as the Adda River, were known as the Terraferma; they were acquired partly as a buffer against belligerent neighbours, partly to guarantee Alpine trade routes, and partly to ensure the supply of mainland wheat (on which the city depended). In building its maritime commercial empire, Venice dominated the trade in salt, acquired control of most of the islands in the Aegean, including Crete, and Cyprus in the Mediterranean, and became a major power-broker in the Near East. By the standards of the time, Venice's stewardship of its mainland territories was relatively enlightened and the citizens of such towns as Bergamo, Brescia, and Verona rallied to the defence of Venetian sovereignty when it was threatened by invaders."
 
-for span in spans:
-    print(span)
+text = """GRAMMAR: Grounded and Modular Evaluation of Domain-Specific Retrieval-Augmented Language Models
+Rumour Evaluation with Very Large Language Models
+A Legal Framework for Natural Language Processing Model Training in Portugal
+Investigating Automatic Scoring and Feedback using Large Language Models
+When Quantization Affects Confidence of Large Language Models
+Better & Faster Large Language Models via Multi-token Prediction
+Self-Refine Instruction-Tuning for Aligning Reasoning in Language Models
+Computational Job Market Analysis with Natural Language Processing
+A Survey of Generative Search and Recommendation in the Era of Large Language Models
+Octopus v4: Graph of language models
+Utilizing Large Language Models to Identify Reddit Users Considering Vaping Cessation for Digital Interventions
+BMRetriever: Tuning Large Language Models as Better Biomedical Text Retrievers
+ChatGPT Is Here to Help, Not to Replace Anybody -- An Evaluation of Students' Opinions On Integrating ChatGPT In CS Courses
+Contrastive Learning Method for Sequential Recommendation based on Multi-Intention Disentanglement
+MER 2024: Semi-Supervised Learning, Noise Robustness, and Open-Vocabulary Multimodal Emotion Recognition
+In-Context Learning with Long-Context Models: An In-Depth Exploration
+Structure learning of Hamiltonians from real-time evolution
+Koopman-based Deep Learning for Nonlinear System Estimation
+A Survey on Deep Active Learning: Recent Advances and New Frontiers
+Clover: Regressive Lightweight Speculative Decoding with Sequential Knowledge
+New Benchmark Dataset and Fine-Grained Cross-Modal Fusion Framework for Vietnamese Multimodal Aspect-Category Sentiment Analysis
+CLARE: Cognitive Load Assessment in REaltime with Multimodal Data
+Context-Aware Machine Translation with Source Coreference Explanation
+Retrieval-Augmented Generation with Knowledge Graphs for Customer Service Question Answering
+Retrieval-Oriented Knowledge for Click-Through Rate Prediction
+Gradient-based Automatic Per-Weight Mixed Precision Quantization for Neural Networks On-Chip
+Automatic Creative Selection with Cross-Modal Matching
+Misaka: Interactive Swarm Testbed for Smart Grid Distributed Algorithm Test and Evaluation
+Text Sentiment Analysis and Classification Based on Bidirectional Gated Recurrent Units (GRUs) Model
+Derivative-based regularization for regression
+NLU-STR at SemEval-2024 Task 1: Generative-based Augmentation and Encoder-based Scoring for Semantic Textual Relatedness
+Joint Optimization of Piecewise Linear Ensembles
+A Scoping Review on Simulation-based Design Optimization in Marine Engineering: Trends, Best Practices, and Gaps
+KVP10k : A Comprehensive Dataset for Key-Value Pair Extraction in Business Documents
+Mixture of insighTful Experts (MoTE): The Synergy of Thought Chains and Expert Mixtures in Self-Alignment
+M3oE: Multi-Domain Multi-Task Mixture-of Experts Recommendation Framework
+The Future of Scientific Publishing: Automated Article Generation
+Time Machine GPT
+Interest Clock: Time Perception in Real-Time Streaming Recommendation System
+Humans prefer interacting with slow, less realistic butterfly simulations
+Modeling Orthographic Variation in Occitan's Dialects
+InspectorRAGet: An Introspection Platform for RAG Evaluation
+Edge Importance in Complex Networks
+MMAC-Copilot: Multi-modal Agent Collaboration Operating System Copilot
+A Comprehensive Survey of Dynamic Graph Neural Networks: Models, Frameworks, Benchmarks, Experiments and Challenges
+GenAI Distortion: The Effect of GenAI Fluency and Positive Affect
+Winning the Social Media Influence Battle: Uncertainty-Aware Opinions to Understand and Spread True Information via Competitive Influence Maximization
+A Formal Specification of a Data Model for Malaria Surveillance in the Developing World
+"Actually I Can Count My Blessings": User-Centered Design of an Application to Promote Gratitude Among Young Adults
+Exploring Weighted Property Approaches for RDF Graph Similarity Measure
+Dynamic Human Trust Modeling of Autonomous Agents With Varying Capability and Strategy
+Human-in-the-Loop Synthetic Text Data Inspection with Provenance Tracking
+Capabilities"""
+
+text = '. '.join(text.split('\n'))
+
+for sent in splitter(text):
+    tokens = tokenizer(sent)
+    for n in [3, 1]:
+        for chunk in ngrams(tokens, n):
+            phrase = ' '.join(chunk).lower()
+            for span in refined.process_text(phrase):
+                print(f'{phrase}  ||  {str(span)}')
