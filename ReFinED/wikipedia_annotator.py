df4132b25d74e36e4d6cbed891f499d5aab7da1a
Jordi Carrera Ventura <jordi.carrera.ventura@gmail.com>
2024-05-06 20:31:59 +0200

    Finish term extraction and retrieve Wikipedia pages

--- ReFinED/wikipedia_annotator.py
+++ ReFinED/wikipedia_annotator.py
@@ -1,6 +1,7 @@
-from functools import lru_cache
+from difflib import SequenceMatcher
 import json
 import random
+import re
 import time
 
 from nltk import ngrams
@@ -9,12 +10,13 @@ from nltk import sent_tokenize as splitter
 from nltk import wordpunct_tokenize as tokenizer
 from refined.inference.processor import Refined
 import wikipedia
+from wikipedia.exceptions import DisambiguationError, PageError
 
 from cache import Cache
 
 
 
-WIKIPEDIA_CACHE_SIZE = 5000
+WIKIPEDIA_CACHE_SIZE = 25000
 
 STOPWORDS = stopwords.words('english')
 STOPWORDS.extend([
@@ -23,7 +25,11 @@ STOPWORDS.extend([
     'seven'
 ])
 
-CACHE = Cache(size=WIKIPEDIA_CACHE_SIZE)
+CACHE_PAGES = Cache(size=WIKIPEDIA_CACHE_SIZE, path='cache_pages.p')
+
+CACHE_ARTICLES = Cache(size=WIKIPEDIA_CACHE_SIZE, path='cache_articles.p')
+
+TOKEN = re.compile(r'\w+', re.IGNORECASE)
 
 
 
@@ -43,13 +49,109 @@ def extract_terms(text, orders=(1, 3)):
                 yield ' '.join(gram)
 
 
-def cached_lookup(term):
-    if term in CACHE:
-        #print(term, CACHE.data.keys())
-        return CACHE[term]
+def extract_words(text):
+    terms = []
+    for _match in TOKEN.finditer(text):
+        terms.append({
+            'raw': _match.group(),
+            'text': _match.group().lower(),
+            'start': _match.start(),
+            'end': _match.end(),
+        })
+    return terms
+
+
+
+def filter_categories(categories):
+    return [
+        cat for cat in categories
+        if not (
+            (cat.startswith('Articles ') and 'identifiers' in cat)
+            or (cat.startswith('Articles with '))
+            or (cat.startswith('Articles needing '))
+            or (cat.startswith('Articles containing '))
+            or (cat.startswith('Articles lacking '))
+            or (cat.startswith('Articles using '))
+            or (cat.startswith('Pages using '))
+            or (cat.startswith('Pages with '))
+            or (cat.startswith('CS1 '))
+            or ' stubs' in cat
+            or (cat.startswith('All '))
+            or 'Webarchive template ' in cat
+            or 'Wikipedia' in cat
+            or 'Wikipedia' in cat
+        )
+    ]
+
+
+
+def extract_terms2(text, orders=[4, 3, 2, 1], ratio=0.75):
+    words = extract_words(text)
+    covered = set([])
+    orders = orders.copy()
+    terms = []
+    while orders:
+        order = orders.pop(0)
+        for idx in range(len(words) - order):
+            area = set(range(idx, idx + order))
+            if area.intersection(covered):
+                continue
+
+            tokens = words[idx:idx + order]
+            tokens_text = [token['text'] for token in tokens]
+            first = tokens_text[0]
+            last = tokens_text[-1]
+
+            if first in STOPWORDS \
+            or last in STOPWORDS \
+            or [token for token in tokens_text if not token.isalpha()]:
+                continue
+
+            term = text[tokens[0]['start']:tokens[-1]['end']]
+            term_text = ' '.join(tokens_text)
+            results = cached_lookup(term, CACHE_PAGES, wikipedia.search)
+
+            matched = False
+            for page_title in results:
+                if len(page_title.split()) > len(term_text.split()):
+                    continue
+                matcher = SequenceMatcher(None, page_title.lower(), term_text)
+                if matcher.ratio() >= ratio:
+                    print('\t>>>', term_text, '||', page_title)
+                    try:
+                        article = cached_lookup(term, CACHE_ARTICLES, wikipedia.page)
+                    except DisambiguationError:
+                        continue
+                    except PageError:
+                        continue
+                    terms.append({
+                        'order': order,
+                        'components': tokens,
+                        'wikipedia_page': page_title,
+                        'wikipedia_article': {
+                            'title': article.title,
+                            'url': article.url,
+                            'categories': filter_categories(article.categories),
+                            #'content': article.content,
+                            'summary': article.summary
+                        }
+                    })
+                    covered.update(area)
+                    matched = True
+                    break
+            if not matched:
+                print(term_text)
+
+    return terms
+
+
+
+def cached_lookup(term, cache, func):
+    if term in cache:
+        return cache[term]
     else:
-        results = wikipedia.search(term)
-        CACHE.add(term, results)
+        results = func(term)
+        cache.add(term, results)
     time.sleep(random.randrange(1, 3) + random.uniform(0.0, 1.0))
     return results
 
@@ -92,12 +194,17 @@ class WikipediaAnnotator:
         return annotation
 
 
+
 if __name__ == '__main__':
 
     ann = WikipediaAnnotator()
 
     text = "From the 9th to the 12th centuries, Venice developed into a powerful maritime empire (an Italian thalassocracy known also as repubblica marinara). In addition to Venice there were seven others: the most important ones were Genoa, Pisa, and Amalfi; and the lesser known were Ragusa, Ancona, Gaeta and Noli."
 
+    text = "Its own strategic position at the head of the Adriatic made Venetian naval and commercial power almost invulnerable. With the elimination of pirates along the Dalmatian coast, the city became a flourishing trade centre between Western Europe and the rest of the world, especially with the Byzantine Empire and Asia, where its navy protected sea routes against piracy."
+
     for sent in splitter(text):
-        annotations = ann(sent)
-        print(json.dumps(annotations, indent=4))
+        print()
+        print(sent)
+        for term in extract_terms2(sent):
+            print(json.dumps(term, indent=4))
