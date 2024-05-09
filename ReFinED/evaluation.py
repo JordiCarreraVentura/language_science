14ef1e183d52bf40cb88f1c8012897de999f06e6
Jordi Carrera Ventura <jordi.carrera.ventura@gmail.com>
2024-05-09 10:19:21 +0200

    Implement `diff_annotations` helper function

--- ReFinED/evaluation.py
+++ ReFinED/evaluation.py
@@ -15,6 +15,22 @@ not_incr = lambda x: True if x[0] >= x[1] else False
 
 
 
+def check_annotations(annotations: list[tuple[int, int, str, str]]) -> None:
+    if not isinstance(annotations[0], tuple):
+        raise ValueError('Values stored in `annotations` must be tuples, got '
+                         f'"{annotations[0]}"')
+    starts = [ann[0] for ann in annotations]
+    ends = [ann[1] for ann in annotations]
+    if list(filter(not_int, starts)) \
+    or list(filter(not_int, ends)) \
+    or list(filter(not_incr, [(x, y) for x, y in zip(starts, ends)])):
+        raise ValueError('Values stored in `annotations` must be tuples, '
+                         'the first two elements in the tuple must be '
+                         'integers, and the second integer must be greater '
+                         f'than the first one, but got "{annotations}"')
+
+
+
 def character_coverage(
     text: str,
     annotations: list[tuple[int, int, str, str]]
@@ -89,18 +105,7 @@ def character_coverage(
                          f'"{annotations}"')
     if not annotations:
         return 0.0
-    if not isinstance(annotations[0], tuple):
-        raise ValueError('Values stored in `annotations` must be tuples, got '
-                         f'"{annotations[0]}"')
-    starts = [ann[0] for ann in annotations]
-    ends = [ann[1] for ann in annotations]
-    if list(filter(not_int, starts)) \
-    or list(filter(not_int, ends)) \
-    or list(filter(not_incr, [(x, y) for x, y in zip(starts, ends)])):
-        raise ValueError('Values stored in `annotations` must be tuples, '
-                         'the first two elements in the tuple must be '
-                         'integers, and the second integer must be greater '
-                         f'than the first one, but got "{annotations}"')
+    check_annotations(annotations)
 
     num = 0.0
     denom = len(text)
@@ -112,9 +117,73 @@ def character_coverage(
     return 0.0
 
 
+
+def diff_annotations(
+    ann1: list[tuple[int, int, str, str]],
+    ann2: list[tuple[int, int, str, str]]
+) -> list[list[tuple[int, int, str, str]]]:
+    """TODO: add description
+
+    Parameters
+    ----------
+    ann1: list[tuple[int, int, str, str]]
+        TODO: fill
+    ann2: list[tuple[int, int, str, str]]
+        TODO: fill
+
+    Returns
+    -------
+    list[tuple[int, int, str, str]]
+        Given all elements in either `ann1` or `ann2`, and considering the
+        first and second sub-elements in each element (the two integers
+        denoting `start` and `end` position indexes for each annotation),
+        returns a new set with all elements from either `ann1` or `ann2`
+        whose start and end positions do not match those of any other element
+        in the other set (`ann2` or `ann1` respectively).
+
+    Examples
+    --------
+
+    Raises
+    ------
+
+    """
+    if not ann2:
+        return ann1
+    if not ann1:
+        return ann2
+    check_annotations(ann1)
+    check_annotations(ann2)
+
+    ann1_idxs = set([(start, end) for start, end, _, _ in ann1])
+    ann2_idxs = set([(start, end) for start, end, _, _ in ann2])
+    _intersection = ann1_idxs.intersection(ann2_idxs)
+    _union = ann1_idxs.union(ann2_idxs)
+    _diff = _union - _intersection
+    return list(map(
+        lambda x: True if (x[0], x[1]) in _diff else False,
+        ann1 + ann2
+    ))
+
+
+
+
+
+
+
 def test_character_coverage():
     for text, annotations, metric in character_coverage_tests:
         assert character_coverage(text, annotations) == metric
+
+
+def test_diff_annotations():
+    anns = [
+        [(2, 4, 'ph', 'Phi Eta')],
+        [(0, 2, 'be', 'Beta Epsilon')],
+    ]
+    for ann1 in anns:
+        for ann2 in anns:
+            diff_annotations(ann1, ann2)
 
 
 
@@ -123,6 +192,7 @@ def test_character_coverage():
 
 
 if __name__ == '__main__':
-    test_character_coverage()
-
-    testmod()
+    test_diff_annotations()
+#     test_character_coverage()
+#
+#     testmod()
